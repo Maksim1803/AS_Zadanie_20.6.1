3179951b1a2bac1d217866bd2dc38b853d529245
Maksim1803 <maks1803@yandex.ru>
2025-01-14 18:31:45 +0300

    First commit to the project to git.

---
+++ app/src/main/java/com/example/zadanie2061/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.zadanie2061
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickToast(view: View) {
+        Toast.makeText(this, "Я буду android-разработчиком!", Toast.LENGTH_LONG).show()
+    }
+}
